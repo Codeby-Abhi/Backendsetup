273f0c87efc528cdc2a9f28731ea98015664c04b
Codeby-Abhi <abhi.shobhasana@gmail.com>
2025-05-06 19:45:24 +0530

    multer added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,40 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOncloudinary = async (localFilePath) => {
+    try {
+        // Check if the file exists before uploading if not return null 
+        if (!localFilePath) return null
+        //uploder.upload() method is used to upload the file to cloudinary
+        // and it takes the file path as an argument
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto',
+        })
+        //message to be displayed if the file is uploaded successfully
+        // and it returns the url of the file
+        console.log('File uploaded successfully', response.url);
+        return response;
+    } catch (error) {
+        //it remove the file from local storage if the upload fails
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+const uploadResult = await cloudinary.uploader
+    .upload(
+        'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+    }
+    )
+    .catch((error) => {
+        console.log(error);
+    });
+
+console.log(uploadResult);
