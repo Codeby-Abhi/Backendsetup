56cf9103e7437d42e46b7f49f15e21ad0047bab8
Codeby-Abhi <abhi.shobhasana@gmail.com>
2025-05-08 09:43:00 +0530

    some more changes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,18 @@
-import {asyncHandler} from "../utils/asyncHandler.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
 
-const registerUser = asyncHandler(async(req , res) => {
-    return res.status(200).json({message : "okay...!"})
+const registerUser = asyncHandler(async (req, res) => {
+    // get user details from frontend
+    // validation that its not empty
+    // check if the user already exists in the database
+    // check for image,avatar
+    // upload the image to cloudinary
+    // create the user object because we are using mongoose a nosql database
+    // remove pass and refresh token from response
+    // check for user creation
+    // send the response to the frontend
+
+    const {fullName, email, useName, password} = req.body
+    console.log("email : ", email);
 })
 
 export { registerUser };
--- src/middelwears/multer.middelwear.js
+++ src/middelwears/multer.middelwear.js
@@ -1,6 +1,9 @@
 import multer from "multer";
 
 //storage will be used as middleware to store the file in the local storage
+//multer is a middleware for handling multipart/form-data, which is used for uploading files
+//multer.diskStorage is used to store the file in the local storage
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, './public/temp')
